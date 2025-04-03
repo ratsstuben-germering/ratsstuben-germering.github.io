c50b3b41cfaea9d3c9173e70a79109dc29bb5c54
ratsstuben-germering <ratsstuben.germering@gmail.com>
2025-04-03 20:06:30 +0200

    Podrska za telegram - doradena deply skr12121

--- php/Tischreservierung.php
+++ php/Tischreservierung.php
@@ -1,5 +1,5 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     session_start();
     $_SESSION['reservation'] = [
         'nastanak_rezervacije' => date('H-i d-m-Y'),
